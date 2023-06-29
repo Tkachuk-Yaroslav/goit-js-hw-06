bda1cf917e91291f998f96899dc793501684e3c5
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-06-29 20:07:45 +0300

    завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,4 @@
+//маємо початково
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +7,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+//спочатку знайду список за айдішніком
+const ingredientsElem = document.querySelector("#ingredients");
+
+
+const itemElem = ingredients.map((ingredient) => {
+  //під час кожного проходження циклу створюю лішку
+  const item = document.createElement("li");
+  //добавляю текстовий контент в лішку
+  item.textContent = ingredient;
+  //добавляю лішці клас
+  item.classList.add("item");
+  return item;
+});
+
+// console.log(itemElem);
+//розпиляю масив
+ingredientsElem.append(...itemElem)
