51c0368241f75a13d29ad7e82674ce385a8a9a21
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-07-01 12:58:43 +0300

    завдання 7

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+//ловлю інпут та спан
+const inputElem = document.querySelector("input");
+const spanElem = document.querySelector("span");
+//добавляю слухача подій на подію інпута
+inputElem.addEventListener("input", onChangeText);
+//колбек для слухача подій
+function onChangeText(event) {
+    //розмір тексту спана = значенню текущього елемента
+    spanElem.style.fontSize = `${event.currentTarget.value}px`;
+}
