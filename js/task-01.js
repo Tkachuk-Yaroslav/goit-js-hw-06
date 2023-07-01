7abfd87b98dd786bde8d2a17ba65403b3fb4b59f
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-07-01 22:24:05 +0300

    завдання 1 апдейт

--- js/task-01.js
+++ js/task-01.js
@@ -3,23 +3,13 @@ console.log(`Number of categories: ${categoriesElemList.length}`);
 
 
 categoriesElemList.forEach((category) => {
-    const categoryTitle = category.querySelector("h2");
-    const categoryName = categoryTitle.textContent;
-  //Усередині функції category => { ... } ми отримуємо назву 
-  //категорії за допомогою category.querySelector('h2');. 
-  //За допомогою category.querySelector('h2'), ми шукаємо 
-  //перший елемент < h2 >, який є дочірнім для поточної 
-  //категорії.Використання.textContent дозволяє отримати 
-  //текстовий вміст цього елемента.
-    const categoryElements = category.querySelectorAll("li");
-    const categoryCount = categoryElements.length;
-  //Ми також використовуємо category.querySelectorAll('li'), 
-  //щоб знайти всі елементи < li >, які є дочірніми 
-  //для поточної категорії, і потім отримати їх кількість за
-  //допомогою.length.
-  console.log(`Category : ${categoryName}`);
-  //Для кожної категорії ми використовуємо console.log для 
-  //виведення інформації про категорію та кількість елементів.
-  //Використовується рядок шаблону для побудови повідомлення.
+  const categoryTitle = category.firstElementChild.textContent;
+  // const categoryTitle = category.querySelector("h2");
+  // const categoryName = categoryTitle.textContent;
+  const categoryElements = category.lastElementChild.children;
+  // const categoryElements = category.querySelectorAll("li");
+  const categoryCount = categoryElements.length;
+  
+  console.log(`Category : ${categoryTitle}`);
   console.log(`Elements: ${categoryCount}`);
 });
