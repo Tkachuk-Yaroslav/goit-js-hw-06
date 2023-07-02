9045e76ef99b26feef3d5ef83ec69db79dfddd8c
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-07-03 00:29:25 +0300

    завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+//ловлю боді елемент, кнопку і спан
+const bodyElem = document.querySelector("body");
+const btnElem = document.querySelector(".change-color");
+const colorTextElem = document.querySelector(".color");
+//ставлю слухача на кнопку
+btnElem.addEventListener("click", onButtonCkick);
+//колбек для слухача
+function onButtonCkick() {
+  //в змінну присвоюю значення яке згенериться в getRandomHexColor
+  const bodyBackgroundColor = getRandomHexColor();
+  bodyElem.style.backgroundColor = `${bodyBackgroundColor}`;
+  //вивожу колір в RGB форматі або в HEX форматі(закоментовано)
+  colorTextElem.textContent = bodyElem.style.backgroundColor;
+  // colorTextElem.textContent = getRandomHexColor();
+}
+
+
