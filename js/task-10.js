99c31ecd08d19d0bbe70a4a12d4e881ad5d26b07
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-07-03 01:25:46 +0300

    завдання 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,61 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+//ловлю кнопки, інпут та дів куди вставлятиму квадрати
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const inputElem = document.querySelector("input");
+const divBoxes = document.querySelector("#boxes");
+
+//функція яка створює і добавляє квадрати
+function createBoxes(amount) {
+  //створюю пустий DocumentFragment
+  const divFragment = document.createDocumentFragment();
+  //початкове значення довжини і ширини діва
+  const startSizeDiv = 30;
+  
+  for (let i = 0; i < amount; i++) {
+    //оновлюю значення ширини і довжини
+    const sizeDiv = startSizeDiv + i * 10;
+    //створюю дів
+    const divElement = document.createElement("div");
+    //рандомний колір
+    const foneColor = getRandomHexColor();
+    //присвоюю ширину і довжину діва
+    divElement.style.width = `${sizeDiv}px`;
+    divElement.style.height = `${sizeDiv}px`;
+    //присвоюю фон діва
+    divElement.style.backgroundColor = `${foneColor}`;
+    //додаю дів до DocumentFragment
+    divFragment.appendChild(divElement); //--------
+  }
+
+  //додаю DocumentFragment в розмітку
+  divBoxes.appendChild(divFragment);
+}
+
+//функція яка чистить екран
+function destroyBoxes() {
+  //чищу вміст діва в якому знаходяться створені діви
+  divBoxes.innerHTML = '';
+  //чищу значення інпута
+  inputElem.value = '';
+}
+
+//ставлю слухача на кнопку
+btnCreate.addEventListener('click', onClickCreateBtn);
+//калбек
+function onClickCreateBtn() {
+  //змінна яка визначає скільки дівів буде створено
+  const count = Number(inputElem.value);
+  //виклик функції яка створює діви
+  createBoxes(count);
+}
+
+//ставлю слухача на кнопку
+btnDestroy.addEventListener("click", onClickDestroyBtn);
+//калбек
+function onClickDestroyBtn() {
+  //виклик функції яка чистить екран
+  destroyBoxes();
+}
